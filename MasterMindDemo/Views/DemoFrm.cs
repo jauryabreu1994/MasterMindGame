816461925a3e8594b6b50ed699a6acc6bebc0031
Jaury Nicolás Abreu <jabreu@agilisa.com>
2021-02-05 10:23:50 -0400

    Comments in Main Form

--- MasterMindDemo/Views/DemoFrm.cs
+++ MasterMindDemo/Views/DemoFrm.cs
@@ -6,11 +6,26 @@ using System.Windows.Forms;
 
 namespace MasterMindDemo
 {
+    /**
+     * For our exercise, we will build a function that takes a list of 4 colors, and returns a list of 0‐4 
+     * black/white results.
+     * 
+     * Part of this exercise is reading comprehension and domain discovery, so we won't outline all forms of
+     * input and output here. Read the rules above (and the linked post) to familiarize yourself with the game's
+     * rules, so that your implementation is consistent with the game's rules.
+     * 
+     * The objective is to compare the actual color‐code to the guessed color‐code, and return the correct
+     * amount of black/white pegs (based on the game's rules).
+     * 
+     * Your implementation should be as efficient as possible, clean and easy to read & understand.
+     *
+     **/
     public partial class DemoFrm : Form
     {
         private MasterEntity masterCombination = new MasterEntity();
         private MasterEntity currentLine = new MasterEntity();
         private List<ValidationEntity> resultLines = new List<ValidationEntity>();
+        private bool showCombination = false;
         public DemoFrm()
         {
             InitializeComponent();
@@ -29,6 +44,8 @@ namespace MasterMindDemo
             cmbPosition_SelectedIndexChanged(null, null);
         }
 
+
+        //Add the color to the object 
         private void SaveLine(MasterEntity line, int button) 
         {
 
@@ -51,6 +68,7 @@ namespace MasterMindDemo
             cmbPosition.SelectedIndex = (cmbPosition.SelectedIndex != 3) ? cmbPosition.SelectedIndex + 1 : 0;
         }
 
+        //Validate if the 4 colors have been added
         private bool LineValidationIsComplete(MasterEntity line) 
         {
             if (line.FirstPosition.Equals(CodePeg.None) ||
@@ -63,12 +81,15 @@ namespace MasterMindDemo
                 return true;
         }
 
+        //Validate to send multiple options
         private void btnMain_Click(object sender, System.EventArgs e)
         {
+            //When the game is over, restart the game
             if (!cmbPosition.Enabled) 
             {
                 ResetApp();
             }
+            //Check if the initial combination is complete, to proceed to guess
             else if (LineValidationIsComplete(masterCombination) && !masterCombination.IsComplete)
             {
                 masterCombination.IsComplete = true;
@@ -78,6 +99,7 @@ namespace MasterMindDemo
                 cmbPosition.SelectedIndex = 0;
                 cmbPosition_SelectedIndexChanged(null, null);
             }
+            //Validates if the current combination of the player to guess is complete
             else if (LineValidationIsComplete(currentLine) && !currentLine.IsComplete)
             {
 
@@ -98,6 +120,7 @@ namespace MasterMindDemo
 
                         if (resultLines[resultLines.Count - 1].IsEqual)
                         {
+                            showCombination = true;
                             btnShow_Click(null,null);
                             gbColor.Enabled = cmbPosition.Enabled = false;
                             btnMain.Text = "Reset";
@@ -106,7 +129,7 @@ namespace MasterMindDemo
                 }
             }
         }
-
+        
         private void cmbPosition_SelectedIndexChanged(object sender, System.EventArgs e)
         {
             if (!LineValidationIsComplete(masterCombination) || !masterCombination.IsComplete)
@@ -114,7 +137,7 @@ namespace MasterMindDemo
             else
                 LoadText(currentLine);
         }
-
+        //Displays the information of the selected in the color field
         private void LoadText(MasterEntity line) 
         {
             switch (cmbPosition.SelectedIndex)
@@ -133,7 +156,7 @@ namespace MasterMindDemo
                     break;
             }
         }
-
+        //Shows the history of combinations made to guess in the game
         private void ShowHistory() 
         {
             txtHistory.Text = string.Empty;
@@ -150,6 +173,7 @@ namespace MasterMindDemo
             }
         }
 
+        //Reset the game
         private void ResetApp() 
         {
             gbColor.Enabled = cmbPosition.Enabled = true;
@@ -160,11 +184,12 @@ namespace MasterMindDemo
             masterCombination = new MasterEntity();
             currentLine = new MasterEntity();
             resultLines = new List<ValidationEntity>();
+            showCombination = false;
         }
-
+        //Show or Hide the secret combination to guess
         private void btnShow_Click(object sender, System.EventArgs e)
         {
-            if (LineValidationIsComplete(masterCombination))
+            if (LineValidationIsComplete(masterCombination) || showCombination)
             {
                 if (string.IsNullOrEmpty(txtCombination.Text))
                 {
